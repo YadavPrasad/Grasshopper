1d610e8ba8397d75311428d93d7eb89790e2a566
Yadav Prasad G B <141987752+YadavPrasad@users.noreply.github.com>
2025-10-24 22:04:16 +0530

    Refactor background image handling in LandingPage

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,17 +1,18 @@
-import Navbar from '../components/Navbar'
+import Background from '../assets/Background.png';
+import Navbar from '../components/Navbar';
 
 function LandingPage() {
   return (
     <div
       style={{
-        backgroundImage: "url('/src/assets/Background.png')",
+        backgroundImage: `url(${Background})`,
         height: '100vh',
-        width : '100vw',
+        width: '100vw',
         backgroundSize: 'cover',
         backgroundPosition: 'center'
       }}
     >
-    <Navbar />
+      <Navbar />
     </div>
   );
 }
